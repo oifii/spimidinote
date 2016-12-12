8ce3cd15fc5fe704a444041d3118fd885ac9e9d2
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 02:21:10 -0500

    first commit

---
+++ Release/spimidinote.bat
@@ -0,0 +1,10 @@
+rem spimidinote -d "Microsoft GS Wavetable SW Synth" 0 60 1000 100
+rem spimidinote -d "Out To MIDI Yoke:  1" 0 60 1000 100 
+rem spimidinote -d "Out To MIDI Yoke:  1" 0 60 -1 100 
+rem spimidinote "Out To MIDI Yoke:  1" 0 60 -1 100 
+rem
+rem spimidinote -d "Out To MIDI Yoke:  1" -c 0 60 5000 100 
+rem spimidinote -d "Microsoft GS Wavetable SW Synth" 0 60 5000 100
+rem spimidinote -d "Out To MIDI Yoke:  1" 15 60 5000 100
+spimidinote -d "Out To MIDI Yoke:  1" 0 60 5000 100
+rem spimidinote -d "loopMIDI Port 1" 0 60 5000 100
